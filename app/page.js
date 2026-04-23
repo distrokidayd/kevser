bd6fe471ba5058c0514c4a960369c229db3740e2
distrokidayd <distrokidayd@gmail.com>
2026-04-23 14:17:05 -0700

    add homepage

---
+++ app/page.js
@@ -0,0 +1,18 @@
+export default function Page() {
+  return (
+    <main style={{
+      minHeight: "100vh",
+      display: "flex",
+      alignItems: "center",
+      justifyContent: "center",
+      background: "black",
+      color: "white",
+      fontFamily: "Arial"
+    }}>
+      <div style={{ textAlign: "center" }}>
+        <h1>Kevser Publishing House</h1>
+        <p>Altyapı yayında. İlk sürüm hazırlanıyor.</p>
+      </div>
+    </main>
+  );
+}
